3d25c793756e597d5ce7e0c56b3261f80d473068
Framansi <fra.mansi@gmail.com>
2025-11-06 12:48:58 +0100

    AGGIUNTA LINK

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kronos</title>
+</head>
+
+<body>
+    <a href="/"><strong>Homepage</strong></a>
+    <a href="chi-sono"><strong>Sono io</strong></a>
+    <a href="contatti"><strong>Contattami</strong></a>
+<strong>Mi chiamo francesco</strong>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//DOMINIO =http://127.0.0.1:8000/
-//URI= /viola
 
-//DOMINIO + URI
-//URL= http://127.0.0.1:8000/viola
-
-
-
+//https://www.w3schools.com/tags/tag_doctype.asp
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('chi-sono', function () {
-    return 'sono io';
+    return view('about');
 });
 
 Route::get('contatti', function () {
     return 'mandami un messaggio';
 });
-
-//Rotta parametrica
-Route::get('servizi/{nome}', function ($nome) {
-    return 'Il mio servizio è: ' . $nome;
-});
-//chi-sono
-//contatti
-
-// Route::get('/viola', function () {
-
-//     $students = ['Francesco', 'Viola'];
-//     $a = 10;
-//     $b = 15;
-//     //return $a + $b;
-//     return $students[1];
-// });
