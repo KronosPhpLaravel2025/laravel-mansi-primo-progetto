5b47bedc0a0b4b9719228e56181bc82a32bcd9b5
Framansi <fra.mansi@gmail.com>
2025-11-25 11:34:24 +0100

    add api

---
+++ resources/views/components/footer-list.blade.php
@@ -0,0 +1,4 @@
+<a style="color:red" href="{{$link}}">
+    <img src="topo" alt="">
+    !{{$text}}
+</a>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', [PageController::class, 'homepage']);
 Route::get('/chi-sono', [PageController::class, 'about']);
 Route::get('/contatti', [PageController::class, 'contact']);
 Route::get('/servizi', [PageController::class, 'services']);
 Route::get('/dettaglio-servizio/{service}', [PageController::class, 'service']);
+
+Route::get('/dammi-il-pokemon/{name}', function ($name) {
+    $data = Http::get('https://pokeapi.co/api/v2/pokemon/' . $name)->json();
+
+    return view('example', ['data' => $data]);
+});
